23f904748d13f10305fc94574185cb0362ce862b
twowin_1 <minwoogi@twowincall.com>
2024-02-03 03:15:59 +0900

    20240203-home

---
+++ src/main/java/com/wio/crm/model/Board.java
@@ -0,0 +1,158 @@
+package com.wio.crm.model;
+
+
+public class Board {
+    private String UNO;
+    private String GNO;
+    private String REPLY_DEPTH;
+    private String CAT_GROUP;
+    private String ID;
+    private String PASSWD;
+    private String SUBJECT;
+    private String CONTENT;
+    private String HIT;
+    private String IN_DATE;
+    private String EMPNO;
+    private String CUST_CODE;
+    private String SAUP_NO;
+    private String ATT_FILE;
+    private String EMPNM;
+    private String DELCODE;
+    private String NOCODE;
+
+    public String getUNO() {
+        return UNO;
+    }
+
+    public void setUNO(String UNO) {
+        this.UNO = UNO;
+    }
+
+    public String getGNO() {
+        return GNO;
+    }
+
+    public void setGNO(String GNO) {
+        this.GNO = GNO;
+    }
+
+    public String getREPLY_DEPTH() {
+        return REPLY_DEPTH;
+    }
+
+    public void setREPLY_DEPTH(String REPLY_DEPTH) {
+        this.REPLY_DEPTH = REPLY_DEPTH;
+    }
+
+    public String getCAT_GROUP() {
+        return CAT_GROUP;
+    }
+
+    public void setCAT_GROUP(String CAT_GROUP) {
+        this.CAT_GROUP = CAT_GROUP;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPASSWD() {
+        return PASSWD;
+    }
+
+    public void setPASSWD(String PASSWD) {
+        this.PASSWD = PASSWD;
+    }
+
+    public String getSUBJECT() {
+        return SUBJECT;
+    }
+
+    public void setSUBJECT(String SUBJECT) {
+        this.SUBJECT = SUBJECT;
+    }
+
+    public String getCONTENT() {
+        return CONTENT;
+    }
+
+    public void setCONTENT(String CONTENT) {
+        this.CONTENT = CONTENT;
+    }
+
+    public String getHIT() {
+        return HIT;
+    }
+
+    public void setHIT(String HIT) {
+        this.HIT = HIT;
+    }
+
+    public String getIN_DATE() {
+        return IN_DATE;
+    }
+
+    public void setIN_DATE(String IN_DATE) {
+        this.IN_DATE = IN_DATE;
+    }
+
+    public String getEMPNO() {
+        return EMPNO;
+    }
+
+    public void setEMPNO(String EMPNO) {
+        this.EMPNO = EMPNO;
+    }
+
+    public String getCUST_CODE() {
+        return CUST_CODE;
+    }
+
+    public void setCUST_CODE(String CUST_CODE) {
+        this.CUST_CODE = CUST_CODE;
+    }
+
+    public String getSAUP_NO() {
+        return SAUP_NO;
+    }
+
+    public void setSAUP_NO(String SAUP_NO) {
+        this.SAUP_NO = SAUP_NO;
+    }
+
+    public String getATT_FILE() {
+        return ATT_FILE;
+    }
+
+    public void setATT_FILE(String ATT_FILE) {
+        this.ATT_FILE = ATT_FILE;
+    }
+
+    public String getEMPNM() {
+        return EMPNM;
+    }
+
+    public void setEMPNM(String EMPNM) {
+        this.EMPNM = EMPNM;
+    }
+
+    public String getDELCODE() {
+        return DELCODE;
+    }
+
+    public void setDELCODE(String DELCODE) {
+        this.DELCODE = DELCODE;
+    }
+
+    public String getNOCODE() {
+        return NOCODE;
+    }
+
+    public void setNOCODE(String NOCODE) {
+        this.NOCODE = NOCODE;
+    }
+}
