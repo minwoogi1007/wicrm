2cf79f20d659e79e88cfccec34a0660e9ca228f4
minwoogi1007 <minwoogi@twowincall.com>
2024-04-10 23:43:54 +0900

    20240410-company

--- src/main/resources/static/assets/js/custom/utilities/search/horizontal.js
+++ src/main/resources/static/assets/js/custom/utilities/search/horizontal.js
@@ -1,1 +1,12 @@
-"use strict";var KTSearchHorizontal={init:function(){var e,n;e=document.querySelector("#kt_advanced_search_form").querySelector('[name="tags"]'),new Tagify(e),(n=document.querySelector("#kt_horizontal_search_advanced_link")).addEventListener("click",(function(e){e.preventDefault(),"Advanced Search"===n.innerHTML?n.innerHTML="Hide Advanced Search":n.innerHTML="Advanced Search"}))}};KTUtil.onDOMContentLoaded((function(){KTSearchHorizontal.init()}));
+"use strict";
+var KTSearchHorizontal = {
+    init: function() {
+        var e, n;
+        e = document.querySelector("#kt_advanced_search_form").querySelector('[name="tags"]'), new Tagify(e), (n = document.querySelector("#kt_horizontal_search_advanced_link")).addEventListener("click", (function(e) {
+            e.preventDefault(), "상세조회" === n.innerHTML ? n.innerHTML = "상세조회 숨기기" : n.innerHTML = "상세조회"
+        }))
+    }
+};
+KTUtil.onDOMContentLoaded((function() {
+    KTSearchHorizontal.init()
+}));
